358a9b959b2a5b1ab43ac3a7bc3a0774d9d9bdaf
Ivan <ipkozyrin@gmail.com>
2025-05-26 19:10:01 -0300

    add

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # tradesk
 App for trade and strategies
+
+```bash
+npx prisma migrate dev --name init
+prisma db pull
+yarn prisma:seed
+```
---
+++ prisma/migrations/20250526220528_init/migration.sql
@@ -0,0 +1,191 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT,
+    "role" TEXT NOT NULL DEFAULT 'USER',
+    "settings" TEXT DEFAULT '{}',
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    "deleted_at" DATETIME
+);
+
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "user_id" INTEGER NOT NULL,
+    "token" TEXT NOT NULL,
+    "user_agent" TEXT,
+    "ip_address" TEXT,
+    "last_active_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    "deleted_at" DATETIME,
+    CONSTRAINT "Session_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Portfolio" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    "deleted_at" DATETIME,
+    "user_id" INTEGER NOT NULL,
+    "is_archived" BOOLEAN NOT NULL DEFAULT false,
+    "group_id" INTEGER,
+    "name" TEXT NOT NULL,
+    "descr" TEXT,
+    "state" TEXT,
+    CONSTRAINT "Portfolio_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Portfolio_group_id_fkey" FOREIGN KEY ("group_id") REFERENCES "Group" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Position" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    "deleted_at" DATETIME,
+    "user_id" INTEGER NOT NULL,
+    "type" TEXT NOT NULL,
+    "group_id" INTEGER,
+    "portfolio_id" INTEGER,
+    "strategy_id" INTEGER,
+    "descr" TEXT,
+    CONSTRAINT "Position_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Position_group_id_fkey" FOREIGN KEY ("group_id") REFERENCES "Group" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "Position_portfolio_id_fkey" FOREIGN KEY ("portfolio_id") REFERENCES "Portfolio" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "Position_strategy_id_fkey" FOREIGN KEY ("strategy_id") REFERENCES "Strategy" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    "deleted_at" DATETIME,
+    "user_id" INTEGER NOT NULL,
+    "date" DATETIME NOT NULL,
+    "position_id" INTEGER,
+    "partial_of_id" INTEGER,
+    "fee_of_id" INTEGER,
+    "trade_order_id" INTEGER,
+    "type" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "from_wallet_id" INTEGER,
+    "from_asset_id" INTEGER,
+    "fromValue" DECIMAL,
+    "to_wallet_id" INTEGER NOT NULL,
+    "to_asset_id" INTEGER NOT NULL,
+    "toValue" DECIMAL NOT NULL,
+    "note" TEXT,
+    CONSTRAINT "Transaction_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Transaction_position_id_fkey" FOREIGN KEY ("position_id") REFERENCES "Position" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "Transaction_partial_of_id_fkey" FOREIGN KEY ("partial_of_id") REFERENCES "Transaction" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "Transaction_fee_of_id_fkey" FOREIGN KEY ("fee_of_id") REFERENCES "Transaction" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "Transaction_trade_order_id_fkey" FOREIGN KEY ("trade_order_id") REFERENCES "TradeOrder" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "Transaction_from_wallet_id_fkey" FOREIGN KEY ("from_wallet_id") REFERENCES "Wallet" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "Transaction_to_wallet_id_fkey" FOREIGN KEY ("to_wallet_id") REFERENCES "Wallet" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Transaction_from_asset_id_fkey" FOREIGN KEY ("from_asset_id") REFERENCES "Asset" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "Transaction_to_asset_id_fkey" FOREIGN KEY ("to_asset_id") REFERENCES "Asset" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Strategy" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    "deleted_at" DATETIME,
+    "user_id" INTEGER NOT NULL,
+    "is_archived" BOOLEAN NOT NULL DEFAULT false,
+    "group_id" INTEGER,
+    "name" TEXT NOT NULL,
+    "descr" TEXT,
+    "state" TEXT,
+    CONSTRAINT "Strategy_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Strategy_group_id_fkey" FOREIGN KEY ("group_id") REFERENCES "Group" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Group" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "type" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "descr" TEXT
+);
+
+-- CreateTable
+CREATE TABLE "Asset" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "ticker" TEXT NOT NULL,
+    "type" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "TradeOrder" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    "deleted_at" DATETIME,
+    "user_id" INTEGER NOT NULL,
+    "remote_order_id" TEXT,
+    "open_date" DATETIME,
+    "fill_date" DATETIME,
+    "cancel_date" DATETIME,
+    "expiration_date" DATETIME,
+    "from_wallet_id" INTEGER NOT NULL,
+    "from_asset_id" INTEGER NOT NULL,
+    "fromValue" DECIMAL NOT NULL,
+    "to_wallet_id" INTEGER NOT NULL,
+    "to_asset_id" INTEGER NOT NULL,
+    "toValue" DECIMAL NOT NULL,
+    "lot_size" DECIMAL,
+    "price" DECIMAL,
+    "action" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "note" TEXT,
+    "data" TEXT,
+    "position_id" INTEGER,
+    CONSTRAINT "TradeOrder_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "TradeOrder_position_id_fkey" FOREIGN KEY ("position_id") REFERENCES "Position" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "TradeOrder_from_wallet_id_fkey" FOREIGN KEY ("from_wallet_id") REFERENCES "Wallet" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "TradeOrder_to_wallet_id_fkey" FOREIGN KEY ("to_wallet_id") REFERENCES "Wallet" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "TradeOrder_from_asset_id_fkey" FOREIGN KEY ("from_asset_id") REFERENCES "Asset" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "TradeOrder_to_asset_id_fkey" FOREIGN KEY ("to_asset_id") REFERENCES "Asset" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Wallet" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    "deleted_at" DATETIME,
+    "user_id" INTEGER NOT NULL,
+    "is_archived" BOOLEAN NOT NULL DEFAULT false,
+    "external_entity_id" INTEGER,
+    "name" TEXT NOT NULL,
+    "descr" TEXT,
+    "state" TEXT,
+    CONSTRAINT "Wallet_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Wallet_external_entity_id_fkey" FOREIGN KEY ("external_entity_id") REFERENCES "ExternalEntity" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "ExternalEntity" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "trademark_name" TEXT NOT NULL,
+    "type" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_token_key" ON "Session"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Asset_ticker_key" ON "Asset"("ticker");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ExternalEntity_trademark_name_key" ON "ExternalEntity"("trademark_name");
